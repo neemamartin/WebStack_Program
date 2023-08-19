f1ca6b92ac7ef4341ad8d132fd472e5eab291395
neemamartin <neemamartin13@gmail.com>
2023-08-19 20:56:44 +0530

    lab_4

---
+++ LAB 4/validate_xml.py
@@ -0,0 +1,10 @@
+import xmlschema
+xml_file = "empolyee.xml"
+xsd_file = "employee_schema.xsd"
+
+validator = xmlschema.XMLSchema(xsd_file)
+if validator.is_valid(xml_file):
+    print("XML file is valid against the XSD schema.")
+else:
+    print("XML file is not valid against the XSD schema.")
+    print(validator.validate(xml_file))
